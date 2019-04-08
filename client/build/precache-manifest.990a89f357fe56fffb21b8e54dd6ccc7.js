7627dbfde1e1287131fd493043703631062ccbed
Michael Klein <miketheklein@gmail.com>
2019-04-08 13:27:19 -0400

    pushing up new config to try and figure out heroku woes

---
+++ client/build/precache-manifest.990a89f357fe56fffb21b8e54dd6ccc7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0d902c7f568cb788d9f",
+    "url": "/static/css/main.34937102.chunk.css"
+  },
+  {
+    "revision": "a0d902c7f568cb788d9f",
+    "url": "/static/js/main.8cc4a52c.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "84adee782a8d465cbee9",
+    "url": "/static/js/2.5fd1c7fb.chunk.js"
+  },
+  {
+    "revision": "3c22dd4ed2a1fc0f01b053e44a69fb4a",
+    "url": "/index.html"
+  }
+];
